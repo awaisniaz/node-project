82a1439336f2248ae82e8eab090cd57ef8fc86a6
mw-awais.niaz <awais.niaz@mwanmobile.org>
2022-01-05 21:25:03 +0500

    Routing Added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var http = require("http")
+import http, { createServer } from "http"
 
 console.log(http)
-http.createServer((req,res)=>{
+createServer((req,res)=>{
     res.write("I am Awais Niaz")
     res.end()
-}).listen(3000)
+}).listen(3000) 
